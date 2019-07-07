56df25c1f8afb4eceb6ddb90755f08db7f676725
未来可期 <1163220839@qq.com>
2019-07-07 15:44:46 +0800

    首页列表样式

--- src/pages/home/advert.vue
+++ src/pages/home/advert.vue
@@ -15,14 +15,27 @@
         <img :src="advertdata.five_example.ads_info.ad_code" alt srcset />
       </a>
     </div>
-    <div>
-      <ul v-for="article_list in advertdata.article_list" :key="article_list.ads_info.ad_id">
-        111
-        <img :src="article_list.ads_info.ad_code" alt srcset />
-        <li v-for="product_info in article_list.ads_info.product_info" :key="product_info.id">
-          <img :src="product_info.product_thumb" alt srcset />
-        </li>
-      </ul>
+    <div class="list">
+      <div v-for="article_list in advertdata.article_list" :key="article_list.ads_info.ad_id" >       
+        
+        <div class="listitem">
+          <img :src="article_list.ads_info.ad_code" alt srcset />
+          <div>
+            <h3>{{article_list.ads_info.ad_name}}</h3>
+          <p>{{article_list.ads_info.ad_title}}</p>
+          </div>
+          
+        </div>
+        <div class="nav">
+          <ul>
+            <li v-for="product_info in article_list.ads_info.product_info" :key="product_info.id" :id="product_info.id" @click="handleclick">
+              <img :src="product_info.product_thumb" alt srcset />
+              <p>{{product_info.ad_title}}</p>
+              <span>{{product_info.ad_subtitle}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -50,6 +63,7 @@ export default {
 .contain 
   .single 
     width 100% 
+    margin-bottom .1rem
     a 
       width 100%
       img 
@@ -58,8 +72,53 @@ export default {
     width 100%
     display flex
     flex-wrap wrap
-    li:nth-child(n+2)
-      width 25%
-    img 
+    li 
+      margin-bottom .1rem
       width 100%
+      img 
+        width 100%
+    li:nth-child(n+2)
+      width 23%
+      margin-left 2%
+
+   .list
+     background #fff
+     .listitem
+      >img 
+        width 100%
+      >div
+        margin  .18rem .2rem 0 .2rem
+        border-bottom solid .5px #000
+        padding-bottom .2rem
+        h3 
+          font-size .14rem
+          include ellipsis(null, 1);
+          padding-bottom .18rem
+        p 
+          font-size .12rem
+    .nav 
+      overflow hidden
+      padding .2rem .1rem
+
+      ul 
+        width 100%
+        display flex
+        overflow auto
+        align-items center 
+
+        li 
+          flex-shrink 0
+          width 1.3rem
+          height 1.8rem
+          text-align center
+          span 
+            font-size .12rem
+            color #999
+          p
+            font-size .13rem
+            color #333
+          img 
+            height 80%
+           
+   
 </style>
